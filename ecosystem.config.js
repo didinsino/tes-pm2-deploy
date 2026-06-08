47e8f49de70aacdb6e88f6e4a850583100f3dbfc
Mohamad Zuhdi Sino <zuhdi.sino@ahu.go.id>
2026-06-08 14:21:22 +0700

    update eco 5

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,7 +22,7 @@ module.exports = {
       repo: "https://github.com/didinsino/tes-pm2-deploy.git",
       path: "/home/ti-didin/test/my-hono-app",
       "pre-deploy-local": "",
-      "post-deploy": "/home/ti-didin/.bun/bin/bun install && /home/ti-didin/.nvm/versions/node/v24.4.0/bin/pm2 reload ecosystem.config.js --env production",
+      "post-deploy": "/home/ti-didin/.bun/bin/bun install && /home/ti-didin/.nvm/versions/node/v24.4.0/bin/pm2 start bun --name my-hono-app --interpreter bun -- run src/index.ts || /home/ti-didin/.nvm/versions/node/v24.4.0/bin/pm2 restart my-hono-app",
       "pre-setup": "",
     },
   },
