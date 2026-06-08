b83591f4b92c451fa0fed6b273244c22ea1627a5
Mohamad Zuhdi Sino <zuhdi.sino@ahu.go.id>
2026-06-08 13:40:02 +0700

    initial

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  apps: [
+    {
+      name: "my-hono-app",
+      script: "bun",
+      args: "run src/index.ts",
+      interpreter: "none",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "ti-didin",
+      host: "192.168.191.103",
+      port: "2222",
+      ref: "origin/main",
+      repo: "git@github.com:didinsino/tes-pm2-deploy.git", // ganti dengan repo kamu
+      path: "/home/ti-didin/test/my-hono-app",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "bun install && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
+};
